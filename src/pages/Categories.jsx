f8660cb5145327b0d172fb93507a01d692457dd7
Fur1ouS <Fur1ouS@ya.ru>
2026-04-09 01:19:12 +0300

    create filter price and add useSearchParams

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,27 +1,41 @@
 import {products} from "../data/data.js";
-import {Link, useParams} from "react-router-dom";
+import {Link, useParams, useSearchParams} from "react-router-dom";
 
 export default function Categories() {
+  const [searchParams, setSearchParams] = useSearchParams()
   const cat = useParams()
-  const categoryArray = products.filter(product => product.categoryId.toLowerCase() === cat.categoriesId)
+
+  const maxPrice = searchParams.get('maxValue') ? searchParams.get('maxValue') : Infinity;
+
+  const categoryArray = products.filter(product => product.categoryId.toLowerCase() === cat.categoriesId && product.price <= +maxPrice);
+
+  function searchPrice(e) {
+    const value = e.target.value;
+    setSearchParams(value ? {maxValue: value} : {})
+  }
 
   return (
     <div>
-      {categoryArray.length > 0 ? <>
         <h1>Category {cat.categoriesId}</h1>
+
+        <div>
+          <input
+            type="number"
+            value={searchParams.get('maxValue') || ""}
+            onChange={searchPrice}/>
+        </div>
+
         <ul className="category">
           {categoryArray.map(item =>
             <li key={item.id}>
               <Link to={`/product-details/${item.id}`} className="category-item">
                 <img src={item.img} alt={item.name}/>
                 <span>{item.name}</span>
-                <span>{item.price}</span>
+                <span>{item.price} $</span>
               </Link>
             </li>
           )}
         </ul>
-      </> : <p>404 page not found</p>}
-
     </div>
   )
 }
